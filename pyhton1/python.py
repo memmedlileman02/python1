d3c46741287abc3527b30cd92ae5fe0bc12be17e
memmedlileman02 <161588533+memmedlileman02@users.noreply.github.com>
2024-05-03 13:56:14 +0400

    Add files via upload

---
+++ pyhton1/python.py
@@ -0,0 +1,33 @@
+# newList = []  
+# for i in range(5):
+#     eded = int(input(f"{i+1}: eded: ")) 
+#     newList.append(eded)  
+# ededler = sorted(newList)
+# print(ededler)
+
+
+
+# word = input("Sözü daxil edin: ")  
+# words = sorted(word)                
+# sort_word = '' . join( words)        
+# print(sort_word)                 
+
+
+
+  
+# for i in range(1,100 ):  
+#    if i > 1:  
+#        for j in range(2,i):  
+#            if (i % j) == 0:  
+#                break  
+#        else:  
+#            print(i) 
+
+
+
+# eded  = 0  
+# say = 0 
+# while eded != 13:  
+#     eded = int(input("Bir ədəd daxil edin: "))  
+#     say += 1 
+# print(f"{say}  cehde tapdiniz.")
